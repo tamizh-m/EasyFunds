d7cc8f4e7a81c151a6a5c1972ddfc3aafb773b02
tamizh-m <tamizhh1997@gmail.com>
2021-06-08 01:03:46 +0530

    Added print statement

--- EasyFunds/src/main/java/com/EasyFunds/Controller/LoginController.java
+++ EasyFunds/src/main/java/com/EasyFunds/Controller/LoginController.java
@@ -17,6 +17,7 @@ public class LoginController {
 	@RequestMapping(value = "/login")	
 public String showLogin(ModelMap model) {
 		model.addAttribute("LoginUser", new User());
+		System.out.println("I have added a print statement");
 	return "login";
 	
 }
